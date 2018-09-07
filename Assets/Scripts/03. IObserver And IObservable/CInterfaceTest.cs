201d115a60675e4bc224bc86f8abc4b98208dcc6
Brunhild <nechwi1121@gmail.com>
2018-09-07 19:14:19 +0900

    03. IObserver And IObservable 테스트 스크립트  작성

---
+++ Assets/Scripts/03. IObserver And IObservable/CInterfaceTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class CInterfaceTest<T> : MonoBehaviour, IObserver<T>, IObservable<T>
+{
+    #region 인터페이스의 구현해야할 내용
+
+    public void OnCompleted()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnError(Exception error)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnNext(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IDisposable Subscribe(IObserver<T> observer)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
