8d446a0fdfaf60710388d5f4ef7b0944e7f54a54
Brunhild <nechwi1121@gmail.com>
2018-09-07 16:36:24 +0900

    02. OnNext And Subscribe 업로드

---
+++ Assets/Scripts/02. OnNext And Subscribe/CSubjectTestRx.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+
+public class CSubjectTestRx : MonoBehaviour {
+
+    public Text logTextUi = null;
+
+    private Subject<string> _testSubject = new Subject<string>();
+
+	// Use this for initialization
+	void Start () {
+
+        // 동작 안함! 구독(Subscribe)이 먼저 구현되지 않았기 때문에 메시지를 발행해도 반응없음
+        //_testSubject.OnNext("Hello World!");
+        //_testSubject.OnNext("안녕하세요!");
+
+        _testSubject.Subscribe(msg => Debug.Log("Subscribe1 : " + msg));
+        _testSubject.Subscribe(msg => Debug.Log("Subscribe2 : " + msg));
+        _testSubject.Subscribe(msg => Debug.Log("Subscribe3 : " + msg));
+
+        _testSubject.Subscribe(msg => 
+        {
+            if (logTextUi != null)
+            {
+                logTextUi.text += msg + "\n";
+            }
+        });
+
+        _testSubject.OnNext("Hello World!");
+        _testSubject.OnNext("안녕하세요!");
+    }
+}
---
+++ Assets/Scripts/02. OnNext And Subscribe/README.md
@@ -0,0 +1,5 @@
+OnNext 와 Subscribe
+
+Subscribe : 메시지의 수신시 실행할 함수를 등록. 메시지의 수신시의 처리를 등록하는 처리.
+
+OnNext : Subscribe에 등록된 함수에 메시지를 건네주고 실행. 메시지를 발생하고 Subscribe에 등록된 처리를 순서대로 호출해가는 처리.
