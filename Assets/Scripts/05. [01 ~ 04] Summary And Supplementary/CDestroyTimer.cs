3e5d34eec769b605d43eee9a3a0e91799a1dc714
Brunhild <nechwi1121@gmail.com>
2018-09-09 18:38:20 +0900

    UniRx 사용시 메모리 관리에 대한 주의점

---
+++ Assets/Scripts/05. [01 ~ 04] Summary And Supplementary/CDestroyTimer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class CDestroyTimer : MonoBehaviour {
+
+    [SerializeField]
+    private CSubjectDispose _disposeTimer = null;
+    
+    private void Start()
+    {
+        _disposeTimer.OnTimeChanged
+            .Where(x => x == 0)
+            .Subscribe( x =>
+            {
+                transform.position = Vector3.zero;
+                Debug.Log("Timer End : " + x);
+            })
+            // AddTo 가 없으면 Space 키를 누를시 이 오브젝트가 제거되며 
+            // 0초에 MissingReferenceException 이 뜨게된다.
+            // 참조한 CSubjectDispose 내에 아직 해당 Subscribe 가 등록되어 있지만
+            // 오브젝트가 제거되며 구독한 오브젝트의 tranform 을 참조할 수 없기 때문이다.
+            // AddTo는 이 오브젝트가 사라지더라도 자동으로 해당 Subscribe 를 Dispose 해준다.
+            .AddTo(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        // 5초가 지나기 전,후에 Space 키를 누르는 테스트를 위한 입력
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ Assets/Scripts/05. [01 ~ 04] Summary And Supplementary/CSubjectDispose.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UniRx;
+
+public class CSubjectDispose : MonoBehaviour {
+
+    [SerializeField]
+    private int timeLeft = 5;
+    
+    // Dispose Test
+    private Subject<int> integerSubject = new Subject<int>();
+
+    // 메모리 누수 방지 테스트를 위한 타이머
+    private Subject<int> timerSubject = new Subject<int>();
+
+    public IObservable<int> OnTimeChanged {
+        get { return timerSubject; }
+    }
+
+    // Use this for initialization
+    void Start() {
+
+        #region Dispose Test
+        // IDisposable 을 기억
+        IDisposable disposable1 = integerSubject.Subscribe(
+            x => Debug.Log("Subscribe1 : " + x),
+            () => Debug.Log("Completed1")).AddTo(gameObject);
+
+        IDisposable disposable2 = integerSubject.Subscribe(
+            x => Debug.Log("Subscribe2 : " + x),
+            () => Debug.Log("Completed2")).AddTo(gameObject);
+
+        integerSubject.OnNext(1);
+        integerSubject.OnNext(2);
+
+        // 구독 종료
+        // 일부 스트림만 구독을 종료하도록 가능.
+        disposable1.Dispose();
+
+        // 구독이 종료된 Subject 는 메시지 전달이 불가하여 동작하지 않음.
+        // OnCompleted 에 무언가의 처리가 필요한 경우 Dispose 를 해버리면 동작하지 않기때문에 주의.
+        // 이 경우는 disposable1 이 구독 중지됨.
+        integerSubject.OnNext(3);
+        integerSubject.OnCompleted();
+
+        disposable2.Dispose();
+        #endregion
+
+        #region AddTo Test
+        StartCoroutine(TimerCoroutine());
+        timerSubject.Subscribe(x => Debug.Log(x));
+        #endregion
+    }
+
+    IEnumerator TimerCoroutine()
+    {
+        yield return null;
+
+        int time = timeLeft;
+
+        while (time >= 0)
+        {
+            timerSubject.OnNext(time);
+            --time;
+            yield return new WaitForSeconds(1.0f);
+        }
+        timerSubject.OnCompleted();
+    }
+}
